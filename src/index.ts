aaef935306f68c59462647095a269daa1f1374af
Severus7 <joshuasapz@gmail.com>
2022-05-04 21:02:52 +0800

    Added loader

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics } from "pixi.js";
+import { Application, Loader } from "pixi.js";
 
 const app = new Application({
   width: innerWidth,
@@ -7,16 +7,46 @@ const app = new Application({
 
 document.body.appendChild(app.view);
 
-const graphics = new Graphics();
-graphics.beginFill(0xffffff); // 1st step
-graphics.drawRect(20, 50, 100, 80); //2nd step
-graphics.endFill(); // 3rd step
+const loader = Loader.shared;
 
-// Another Graphic
-const graphic2 = new Graphics();
-graphic2.beginFill(0xff0000);
-graphic2.drawCircle(50, 100, 30);
-graphic2.endFill();
+// Load assets
+loader
+  .add(
+    "img1",
+    "https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528__480.jpg"
+  )
+  .add(
+    "img2",
+    "https://cdn.pixabay.com/photo/2015/07/19/09/47/still-life-851297__340.jpg"
+  )
+  .load((l, r) => {
+    const img1 = l.resources.img1;
+    const img2 = l.resources.img2;
 
-app.stage.addChild(graphics);
-app.stage.addChild(graphic2);
+    console.log(img1, img2);
+  });
+
+loader.onProgress.add((l, r) => {
+  console.log(loader.progress);
+});
+
+loader.onLoad.add((e) => {
+  console.log(e);
+});
+
+loader.onComplete.add((e) => {
+  console.log("Loading of the assets is completed!");
+});
+// const graphics = new Graphics();
+// graphics.beginFill(0xffffff); // 1st step
+// graphics.drawRect(20, 50, 100, 80); //2nd step
+// graphics.endFill(); // 3rd step
+
+// // Another Graphic
+// const graphic2 = new Graphics();
+// graphic2.beginFill(0xff0000);
+// graphic2.drawCircle(50, 100, 30);
+// graphic2.endFill();
+
+// app.stage.addChild(graphics);
+// app.stage.addChild(graphic2);
