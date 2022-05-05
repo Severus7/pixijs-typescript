a4a0154e8574d55bde6e1e9bbfd97aeab91749d8
Severus7 <joshuasapz@gmail.com>
2022-05-05 20:15:52 +0800

    remove loader

--- src/index.ts
+++ src/index.ts
@@ -10,33 +10,7 @@ document.body.appendChild(app.view);
 const loader = Loader.shared;
 
 // Load assets
-loader
-  .add(
-    "img1",
-    "https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528__480.jpg"
-  )
-  .add(
-    "img2",
-    "https://cdn.pixabay.com/photo/2015/07/19/09/47/still-life-851297__340.jpg"
-  )
-  .load((l, r) => {
-    const img1 = l.resources.img1;
-    const img2 = l.resources.img2;
 
-    console.log(img1, img2);
-  });
-
-loader.onProgress.add((l, r) => {
-  console.log(loader.progress);
-});
-
-loader.onLoad.add((e) => {
-  console.log(e);
-});
-
-loader.onComplete.add((e) => {
-  console.log("Loading of the assets is completed!");
-});
 // const graphics = new Graphics();
 // graphics.beginFill(0xffffff); // 1st step
 // graphics.drawRect(20, 50, 100, 80); //2nd step
